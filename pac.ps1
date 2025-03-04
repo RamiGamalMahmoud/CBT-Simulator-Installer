610f0bc123f6ff3617bb4ae881d9021ae9de81b4
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-03-04 02:11:42 +0200

    Create new release v0.0.10

--- pac.ps1
+++ pac.ps1
@@ -1,6 +1,6 @@
 dotnet publish -c Release  ..\..\CBT\ComputerBasedTestsSimulator\ComputerBasedTestsSimulator\ -r win-x64 -o .\publish
 
-$version = "0.0.9"
+$version = "0.0.10"
 $pack_id = "CBT_Simulator"
 $main_exe = "CBT.exe"
 $icon_path = "yafea.ico"
